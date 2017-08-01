2f72e24073aeea721c74c780297106c635cf0897
Ross117 <RossMorran@hotmail.co.uk>
2017-08-01 12:24:20 +0100

    Get details about current stream, improve styling

--- javascript.js
+++ javascript.js
@@ -1,29 +1,35 @@
 $(window).on("load", () => {
  "use strict";
 
-    let users = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
-    let $userContainer = $(".userContainer");
+    const users = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
+    const $userContainer = $(".userContainer");
     let link = "";
+    let img = "";
+    let html = "";
 
 //  create a loading screen for when page is loading?
 
 //  for each item in the users array, make a call to the twitch.tv API to get the link to the user's twitch.tv channel
     users.map( (val) => {
-
       $.ajax({
 //      use a proxy server to prevent CORS error
         url: "https://thingproxy.freeboard.io/fetch/https://wind-bow.gomix.me/twitch-api/channels/" + val,
         success: (json) => {
           link = json.url;
-          $userContainer.append("<div class=" + val + "><p><a href=" + link + ">" + val + "</a></p></div>");
-//        call function to check if user is currently streaming
+          img = json.logo;
+          html = "<div class='" + val + " userBox'><p><a href=" + link + ">" +
+          val + "</a><img src=" + img + " alt='' class='img-responsive'></p></div>";
+//        link opens in new window?
+          $userContainer.append(html);
+//      call function to check if user is currently streaming, get details
+//      if they are streaming
           checkStreamStatus(val);
         },
         error: () => {
-          $userContainer.append("<div><p>Sorry, something went wrong when we tried to get data about " + val + ".</p></div>");
+          $userContainer.append("<div><p>Sorry, something went wrong when we tried to get data about "
+          + val + ".</p></div>");
         }
       });
-
     });
 
 });
@@ -32,20 +38,33 @@ $(window).on("load", () => {
 function checkStreamStatus (user) {
   "use strict";
 
-  let $userDiv = $("." + user);
+  const $userBox = $("." + user);
+  let html = "";
+  let game = "";
+  let status = "";
+  let viewers = 0;
 
   $.ajax({
     // use a proxy server to prevent CORS error
-    url: "https://thingproxy.freeboard.io/fetch/https://wind-bow.gomix.me/twitch-api/streams/" + user,
+    url: "https://thingproxy.freeboard.io/fetch/https://wind-bow.gomix.me/twitch-api/streams/"
+    + user,
     success: (json) => {
+//include visual indicator if channel is on or offline?
       if (json.stream === null) {
-        $userDiv.append("<p>Not currently streaming.</p>");
+        html = "<p>Not currently streaming.</p>"
+        $userBox.append(html);
       } else {
-        $userDiv.append("<p>Currently streaming.</p>");
+        // get details of stream
+        game = json.stream.game;
+        viewers = json.stream.viewers;
+        status = json.stream.channel.status;
+        html = "<p>Currently streaming.</p><p><b>Game:</b> " + game
+        + ";  <b>Status:</b> " + status + ";  <b>Viewers:</b> " + viewers + "</p>"
+        $userBox.append(html);
       }
     },
     error: () => {
-      $userDiv.append("<p>Sorry, something went wrong when we tried to get data about this user.</p>");
+      $userBox.append("<p>Sorry, something went wrong when we tried to get data about this user.</p>");
     }
   });
 
